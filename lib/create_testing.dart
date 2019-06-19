5445f5125cf7e1bb830e2bb06b2f690d09b98f90
Jonas Bornold <jonas.bornold@abilia.se>
2019-06-19 11:21:38 +0200

    Create home page and placehodler for join and create
    Co-authored-by: Tobias Junsten <tobias@junsten.se>

---
+++ lib/create_testing.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CreateTesting extends StatefulWidget {
+  @override
+  _CreateTestingState createState() => _CreateTestingState();
+}
+
+class _CreateTestingState extends State<CreateTesting> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+    );
+  }
+}
