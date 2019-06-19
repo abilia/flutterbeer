b59e917e7fd4f9fe1d4ce20421e52272fed73b89
Tobias Junsten <tobias@junsten.se>
2019-06-19 15:22:22 +0200

    - for to map

--- lib/vote.dart
+++ lib/vote.dart
@@ -3,27 +3,20 @@ import 'package:flutter_redux/flutter_redux.dart';
 import 'package:flutterbeer/state/app_state.dart';
 import 'package:redux/redux.dart';
 
-import 'model/app_model.dart';
-
 class Vote extends StatelessWidget {
-  List<Widget> getAll(AppState appstate) {
-    List<Widget> list = new List<Widget>();
-    for (BeerTasting t in appstate.tastings) {
-      list.add(Text(t.title));
-    }
-    return list;
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Make your votes"),),
+      appBar: AppBar(
+        title: Text("Make your votes"),
+      ),
       body: Column(
         children: <Widget>[
           StoreConnector<AppState, AppState>(
             converter: (Store<AppState> store) => store.state,
             builder: (context, state) {
-              return Column(children: getAll(state));
+              return Column(
+                children: state.tastings.map((t) => Text(t.title)).toList());
             },
           ),
         ],
