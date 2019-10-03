5ece36ef1f3ce40f922a18f9e20e83257579ecc6
Johanna Lynn Lahti <johanna.lynn.lahti@abilia.se>
2019-10-03 13:42:14 +0200

    Add android icon, add android signing config

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 import 'package:flutterbeer/model/app_model.dart';
 
 const BASE_URL = "https://beer.abilia-gbg.se";
-// const BASE_URL = "http://192.168.13.235:8000"
+// const BASE_URL = "http://192.168.13.235:8000";
 
 Future<List<dynamic>> getBeerTastings() async {
   var client = http.Client();
